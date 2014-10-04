621e5dbf87806b3a9ad8645ded817bb5f4c96e8f
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-04 13:56:35 -0700

    Multiple pages can be edited

--- index.elm
+++ index.elm
@@ -7,7 +7,7 @@ import Window
 
 data Command =
   Start |
-  Edit | EndEdit | Update Content
+  Edit Int | Update Int Content | CommitAllEdits
 
 type Page = {
   name:String,
@@ -15,7 +15,9 @@ type Page = {
   editing:Maybe Content
   }
 
-type State = Page
+type State = {
+  pages:[Page]
+  }
 
 commands = Input.input Start
 
@@ -23,35 +25,44 @@ ignore = (Input.input ()).handle
 
 -- Display
 
-renderPage : Page -> Element
-renderPage p = 
+renderPage : Int -> Page -> Element
+renderPage i p = 
   case p.editing of
     Nothing ->
       asText p.name `above`
-      plainText p.content |> Input.clickable commands.handle Edit
+      plainText p.content |> Input.clickable commands.handle (Edit i)
     Just c ->
       asText p.name `above`
-      (Field.field Field.defaultStyle commands.handle (\u -> Update u) "" c |> Input.clickable ignore ())|> color red
+      (Field.field Field.defaultStyle commands.handle (\u -> Update i u) "" c |> Input.clickable ignore ())|> color red
 
 renderScreen : (Int,Int) -> State -> Element
 renderScreen (w,h) s =
-  renderPage s 
-  |> container w h middle |> Input.clickable commands.handle EndEdit
+  flow down (indexedMap renderPage s.pages)
+  |> container w h middle |> Input.clickable commands.handle CommitAllEdits
 
 -- Update
+
+mapAt : Int -> (a -> a) -> [a] -> [a]
+mapAt n fn list = indexedMap (\i -> \o -> if i == n then fn o else o) list
+
+endEditing : Page -> Page
+endEditing p = case p.editing of
+  Just c -> { p | content <- c.string, editing <- Nothing }
+  Nothing -> p
 
 step : Command -> State -> State
 step c s = case c of
   Start -> s
-  Edit -> { s | editing <- Just {string=s.content, selection=Selection 0 0 Forward} }
-  Update c -> { s | editing <- Just c }
-  EndEdit -> case s.editing of
-    Just c -> { s | content <- c.string, editing <- Nothing }
-    Nothing -> s
+  Edit i -> { s | pages <- mapAt i (\p -> { p | editing <- Just {string=p.content, selection=Selection 0 0 Forward} }) s.pages}
+  Update i c -> { s | pages <- mapAt i (\p -> { p | editing <- Just c } ) s.pages }
+  CommitAllEdits -> { s | pages <- map endEditing s.pages }
 
 -- Signals
 
-initial = {name="Welcome", content="fill me", editing=Nothing}
+initial = {pages=[
+  {name="Welcome", content="fill me", editing=Nothing},
+  {name="More info", content="fill me 2", editing=Nothing}
+  ]}
 state = foldp step initial commands.signal
 
 main = renderScreen <~ Window.dimensions ~ state
