661b16b8c1cb190876bab615886334e5ec323b3b
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-04 13:26:33 -0700

    Paragraph can be edited

--- index.elm
+++ index.elm
@@ -1,11 +1,13 @@
 import Graphics.Input as Input
+import Graphics.Input.Field as Field
 import Graphics.Input.Field (Content, Selection, Direction(..))
+import Window
 
 -- Model
 
 data Command =
   Start |
-  Edit
+  Edit | EndEdit | Update Content
 
 type Page = {
   name:String,
@@ -17,6 +19,8 @@ type State = Page
 
 commands = Input.input Start
 
+ignore = (Input.input ()).handle
+
 -- Display
 
 renderPage : Page -> Element
@@ -27,7 +31,12 @@ renderPage p =
       plainText p.content |> Input.clickable commands.handle Edit
     Just c ->
       asText p.name `above`
-      plainText p.content |> color red
+      (Field.field Field.defaultStyle commands.handle (\u -> Update u) "" c |> Input.clickable ignore ())|> color red
+
+renderScreen : (Int,Int) -> State -> Element
+renderScreen (w,h) s =
+  renderPage s 
+  |> container w h middle |> Input.clickable commands.handle EndEdit
 
 -- Update
 
@@ -35,10 +44,14 @@ step : Command -> State -> State
 step c s = case c of
   Start -> s
   Edit -> { s | editing <- Just {string=s.content, selection=Selection 0 0 Forward} }
+  Update c -> { s | editing <- Just c }
+  EndEdit -> case s.editing of
+    Just c -> { s | content <- c.string, editing <- Nothing }
+    Nothing -> s
 
 -- Signals
 
 initial = {name="Welcome", content="fill me", editing=Nothing}
 state = foldp step initial commands.signal
 
-main = renderPage <~ state
+main = renderScreen <~ Window.dimensions ~ state
