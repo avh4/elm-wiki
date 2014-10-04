82ac277d02080b17d3fae0d4a03f70b5559cd5fa
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-04 15:00:05 -0700

    Load page content from github

--- index.elm
+++ index.elm
@@ -2,13 +2,15 @@ import Graphics.Input as Input
 import Graphics.Input.Field as Field
 import Graphics.Input.Field (Content, Selection, Direction(..))
 import Window
+import Http
 
 -- Model
 
 data Command =
   Start |
   Edit Int | Update Int Content | CommitAllEdits |
-  Add
+  Add |
+  Loaded String
 
 type Page = {
   name:String,
@@ -55,7 +57,7 @@ renderScreen (w,h) s =
 mapAt : Int -> (a -> a) -> [a] -> [a]
 mapAt n fn list = indexedMap (\i -> \o -> if i == n then fn o else o) list
 
-newPage = {name="Page", content="", editing=Nothing}
+newPage s = {name="Page", content=s, editing=Nothing}
 
 endEditing : Page -> Page
 endEditing p = case p.editing of
@@ -68,14 +70,22 @@ step c s = case c of
   Edit i -> { s | pages <- mapAt i (\p -> { p | editing <- Just {string=p.content, selection=Selection 0 0 Forward} }) s.pages}
   Update i c -> { s | pages <- mapAt i (\p -> { p | editing <- Just c } ) s.pages }
   CommitAllEdits -> { s | pages <- map endEditing s.pages }
-  Add -> { s | pages <- s.pages ++ [newPage]}
+  Add -> { s | pages <- s.pages ++ [newPage ""]}
+  Loaded content -> { s | pages <- s.pages ++ [newPage content]}
 
 -- Signals
+
+httpToLoadCommand res = case res of
+  Http.Success s -> Loaded s
+  _ -> Start
+
+contentLoad : Signal Command
+contentLoad = httpToLoadCommand <~ Http.send (constant (Http.request "GET" "https://api.github.com/repos/avh4/wiki/contents/blogs.md" "" [("Accept", "application/vnd.github.VERSION.raw")]))
 
 initial = {pages=[
   {name="Welcome", content="fill me", editing=Nothing},
   {name="More info", content="fill me 2", editing=Nothing}
   ]}
-state = foldp step initial commands.signal
+state = foldp step initial (merge commands.signal contentLoad)
 
 main = renderScreen <~ Window.dimensions ~ state
